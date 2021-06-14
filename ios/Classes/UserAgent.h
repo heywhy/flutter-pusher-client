6d79fdd42ecd12695b0db83b8b056ad170b9ed6c
yfer <barabanov.mikle@gmail.com>
2021-06-14 12:29:27 +0300

    Updated PusherSwift to 9.2.2
    and bumped min ios deployment target to 13

--- ios/Classes/UserAgent.h
+++ ios/Classes/UserAgent.h
@@ -1,4 +1,4 @@
 // Generated file, do not edit
 #define LIBRARY_VERSION @"0.3.1"
 #define LIBRARY_NAME @"flutter_pusher_client"
-#define PUSHER_LIBRARY_VERSION @"~> 7.2"
+#define PUSHER_LIBRARY_VERSION @"~> 9.2.2"
